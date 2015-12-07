e2f4ddc12d261d979a2950bea6842ccc198cad82
pratibhap <panchmukh.pratibha2791@gmail.com>
2015-12-07 13:58:06 +0530

    - pagination added to rules. - validation added

---
+++ source/hipara/static/dist/js/rule-import.js
@@ -0,0 +1,26 @@
+$('#rule-upload').validator().on('submit', function(e) {
+    if (e.isDefaultPrevented()) {
+        // handle the invalid form...
+        $('#rule-upload-error b').text('All fields mandatory.');
+        $('#import-error b').text('');
+        return false;
+    } else {
+        // everything looks good!
+        e.isDefaultPrevented();
+        var formdata = $('#rule-upload').serializeArray();
+        console.log($('#rule-upload').serializeArray());
+        if (formdata[1].value == "0") {
+            $('#rule-upload-error b').text('All fields mandatory.');
+            return false;
+        } else {
+            e.isDefaultPrevented();
+            $('#rule-upload-error b').text('');
+            $('#rule-upload').validator('destroy');
+        }
+    }
+});
+
+$('input').on("focus", function() {
+    $('#import-error b').text('');
+    $('#rule-upload-error b').text('');
+});
