34ba4b42126986f9da0069d7b7f66483f23d33ed
jbc22 <brettcu@gmail.com>
2016-03-14 12:29:04 +0000

    continue to delete crap files

--- spikes/old Repo/README.md
+++
@@ -1,2 +0,0 @@
-# Hipara Server
-Web based Manager for Yara Rules built on YaraManager by Kevin Breen (https://github.com/kevthehermit/YaraManager)
--- spikes/old Repo/rule_manager/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from rule_manager.models import Rule, MetaData, RuleStrings, Condition
-
-
-admin.site.register(Rule)
-admin.site.register(MetaData)
-admin.site.register(RuleStrings)
-admin.site.register(Condition)
