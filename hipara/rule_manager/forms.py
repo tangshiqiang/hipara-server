b6752df2edfa0f44eb5a60eaeb4f30841c91a17e
Tin Tam <tin.tam@ge.com>
2016-09-20 13:01:39 -0700

    Adding yara error details to rule upload form

--- hipara/rule_manager/forms.py
+++ hipara/rule_manager/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Category
-
+import sys, linecache
 
 class ImportFile(forms.Form):
     source = forms.CharField(max_length=100, required=True)
@@ -16,6 +16,16 @@ class ImportFile(forms.Form):
 	        file_data = rule_file.read()
 	        rules = yara.compile(source=file_data)
         except:
-        	raise forms.ValidationError("There is syntax error in yar file")
+        	raise forms.ValidationError("There is syntax error in yar file: %s" % PrintException())
         from . import rule_parser
         return rule_parser.split_rules(file_data)
+
+
+def PrintException():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    return 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
