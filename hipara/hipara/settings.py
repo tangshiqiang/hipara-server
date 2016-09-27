d8ea4a97bb929f6fa4bfdf549ec6bef4b53dd154
Tin Tam <tin.tam@ge.com>
2016-09-27 01:20:00 -0700

    Initial add of celery

--- hipara/hipara/settings.py
+++ hipara/hipara/settings.py
@@ -2,78 +2,73 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-try:
-    from secret_key import *
-except ImportError:
-    with open('secret_key.py', 'w') as out:
-        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        from django.utils.crypto import get_random_string
-        out.write("SECRET_KEY = '{0}'".format(get_random_string(50, chars)))
-    from secret_key import *
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEBUG = True
+
+DEMO = True
 
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'registration',
-    'rule_manager',
-    'alert',
-    'settings',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'djcelery',
+	'rest_framework',
+	'registration',
+	'rule_manager',
+	'alert',
+	'settings',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'hipara.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'hipara.wsgi.application'
 
-
 DATABASES = {
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'hipara.sqlite3'),
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '<database_name>',
-        'USER': '<database_user_name>',
-        'PASSWORD': '<database_password>',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+	'sqlite': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(BASE_DIR, 'hipara.sqlite3'),
+	},
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': os.environ['MYSQL_DATABASE'],
+		'USER': os.environ['MYSQL_USER'],
+		'PASSWORD': os.environ['MYSQL_PASSWORD'],
+		'HOST': os.environ['DJANGO_MYSQL_HOST'],
+		'PORT': os.environ['DJANGO_MYSQL_PORT'],
+	}
 }
 
 LANGUAGE_CODE = 'en-us'
@@ -88,7 +83,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+	os.path.join(BASE_DIR, 'static'),
 )
 MEDIA_URL = '/media/'
 
@@ -96,19 +91,36 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = 'staticfiles'
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	'compressor.finders.CompressorFinder',
 )
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'email')
 
-SESSION_COOKIE_NAME = '<cookie_name>'
+SESSION_COOKIE_NAME = os.environ['DJANGO_SESSION_COOKIE_NAME']
 SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 SESSION_COOKIE_AGE = 86400
 
 AUTHENTICATION_BACKENDS = ['django-dual-authentication.backends.DualAuthentication']
 AUTHENTICATION_METHOD = 'both'
 AUTHENTICATION_CASE_SENSITIVE = 'both'
+
+# Celery config
+
+# redis server address
+REDIS_HOST = "redis://%s:%s/0" % (os.environ.get('REDIS_HOST', 'localhost'), os.environ.get('REDIS_PORT', '6379'))
+BROKER_URL = REDIS_HOST
+# store task results in redis
+CELERY_RESULT_BACKEND = REDIS_HOST
+# task result life time until they will be deleted
+CELERY_TASK_RESULT_EXPIRES = 7*86400  # 7 days
+# needed for worker monitoring
+CELERY_SEND_EVENTS = True
+# where to store periodic tasks (needed for scheduler)
+CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
+
+# add following lines to the end of settings.py
+import djcelery
+djcelery.setup_loader()
